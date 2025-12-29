7b56669f157ea2c049602e88418bb4b2e5573b5a
sriharieetyala <sriharieetyala143@gmail.com>
2025-12-29 22:04:22 +0530

    Added enums for Role, UserStatus, Specialization, Department

---
+++ user-service/src/main/java/com/vsms/authservice/enums/Role.java
@@ -0,0 +1,12 @@
+package com.vsms.authservice.enums;
+
+/**
+ * User roles in the VSMS system
+ */
+public enum Role {
+    CUSTOMER,
+    TECHNICIAN,
+    MANAGER,
+    INVENTORY_MANAGER,
+    ADMIN
+}
---
+++ user-service/src/main/java/com/vsms/authservice/enums/Specialization.java
@@ -0,0 +1,12 @@
+package com.vsms.authservice.enums;
+
+/**
+ * Technician specialization types - used for matching with service categories
+ */
+public enum Specialization {
+    GENERAL, // Oil Change, Brake Repair, Tire Change
+    ENGINE, // Engine Repair
+    AC, // AC Service
+    BODY, // Body Work
+    ELECTRICAL // Electrical Repair
+}
