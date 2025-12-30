4a236c57dd1f816c0f68f7133429cb1f7f3cf4b3
sriharieetyala <sriharieetyala143@gmail.com>
2025-12-30 22:46:14 +0530

    added test configuration

---
+++ inventory-service/src/test/java/com/vsms/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsms.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
