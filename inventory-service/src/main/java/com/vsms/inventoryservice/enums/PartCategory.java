ee8a404c442a0922ce32083fdb4c9b8d01e2ded8
sriharieetyala <sriharieetyala143@gmail.com>
2025-12-30 22:40:02 +0530

    added enums for part categories and request status

---
+++ inventory-service/src/main/java/com/vsms/inventoryservice/enums/PartCategory.java
@@ -0,0 +1,14 @@
+package com.vsms.inventoryservice.enums;
+
+public enum PartCategory {
+    ENGINE,
+    BRAKES,
+    ELECTRICAL,
+    SUSPENSION,
+    BODY,
+    TRANSMISSION,
+    FLUIDS,
+    FILTERS,
+    TIRES,
+    OTHER
+}
