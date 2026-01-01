7ac837df4c6dbb8ee733f9a36091850d016a8db7
sriharieetyala <sriharieetyala143@gmail.com>
2026-01-01 23:56:16 +0530

    Create docker-compose with all services and database initialization

---
+++ init-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE vsms_auth_db;
+    CREATE DATABASE vsms_vehicle_db;
+    CREATE DATABASE vsms_service_db;
+    CREATE DATABASE vsms_inventory_db;
+    CREATE DATABASE vsms_billing_db;
+EOSQL
